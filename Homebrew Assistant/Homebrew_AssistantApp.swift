27e30ed6277ad73f588c64b45551aab911b99fe8
Ryan Fahlsing <ryan@fahlsing.com>
2026-04-30 22:38:43 -0400

    Initial Commit

---
+++ Homebrew Assistant/Homebrew_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homebrew_AssistantApp.swift
+//  Homebrew Assistant
+//
+//  Created by Ryan Fahlsing on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct Homebrew_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
